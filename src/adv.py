241f681d62f32de082ba86a92e1cb97b12068346
Samir Gadkari <samir.gadkari@gmail.com>
2019-05-29 12:21:07 -0700

    Done with today's assignment

--- src/adv.py
+++ src/adv.py
@@ -1,26 +1,30 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
 room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+    'outside':
+    Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+    'foyer':
+    Room(
+        "Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+    'overlook':
+    Room(
+        "Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+    'narrow':
+    Room(
+        "Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+    'treasure':
+    Room(
+        "Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
-
 
 # Link rooms together
 
@@ -39,6 +43,33 @@ room['treasure'].s_to = room['narrow']
 
 # Make a new player object that is currently in the 'outside' room.
 
+VALID_MOVES = ['n', 's', 'e', 'w']
+MOVE_PROMPT = f'Type in {",".join(VALID_MOVES)} to move'
+QUIT_PROMPT = ' or \'q\' to quit\''
+INVALID_MOVE = f'Only {VALID_MOVES} are valid moves'
+DONE_MSG = 'Thank you for playing !!'
+
+room = room['outside']
+player = Player(room)
+
+
+def move(room, direction):
+    room_dir = {'n': room.n_to, 's': room.s_to, 'e': room.e_to, 'w': room.w_to}
+    explicit_dir = {
+        'n': 'north',
+        's': 'south',
+        'e': 'east',
+        'w': 'west'
+    }[direction]
+
+    next_room = room_dir[direction]
+    if next_room == None:
+        print(f'\n  ERROR: No room to the {explicit_dir} of "{room.name}"\n')
+        return room
+    else:
+        return next_room
+
+
 # Write a loop that:
 #
 # * Prints the current room name
@@ -49,3 +80,17 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+
+while True:
+    print('Current room>', room)
+    cmd = input(MOVE_PROMPT + QUIT_PROMPT)
+
+    if cmd == 'q':
+        print(DONE_MSG)
+        break
+
+    if cmd not in VALID_MOVES:
+        print(INVALID_MOVE)
+        continue
+
+    room = move(room, cmd)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,19 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+import textwrap
+
+WIDTH = 80
+
+
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        return textwrap.fill(textwrap.dedent(self.name + ':' + self.description + '.') \
+                             .strip(), width=WIDTH)
